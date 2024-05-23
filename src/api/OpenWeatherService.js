ec66e083b22b1d5bc81079cb6656d33d03176a7b
subbuk8977 <chinnodu0987@gmail.com>
2024-05-23 21:45:29 +0530

    first commit

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '402bcda09f16648266d762f35e2414cb';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
